ff391d7c228dbe27ffa3f87be237dc7a7efa6a19
Fahim Ahammed <fahimjjg@gmail.com>
2025-11-16 16:32:43 +0600

    country picker implimented and complete login ui.

--- lib/features/auth/view/login_screen.dart
+++ lib/features/auth/view/login_screen.dart
@@ -1,6 +1,6 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:ia_project/common/widget/error_widget.dart';
-import 'package:ia_project/theme/colors.dart';
+import 'package:ia_project/common/widget/button.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,23 +10,69 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController phoneController = TextEditingController();
+  String selectedCountryCode = ''; // default Bangladesh
+  String selectedCountryFlag = "🇧🇩"; // optional
+
+  @override
+  void dispose() {
+    phoneController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: Text('Enter your Number'), elevation: 0),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Text(
-              'whatsApp will need to verify your phone number',
-              style: TextStyle(fontSize: 12),
-            ),
-            TextButton(onPressed: () {}, child: Text('Pick country')),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'whatsApp will need to verify your phone number',
+                style: TextStyle(fontSize: 12),
+              ),
+              TextButton(
+                onPressed: () {
+                  showCountryPicker(
+                    context: context,
+                    showPhoneCode: true, // shows +91, +880 etc.
+                    onSelect: (Country country) {
+                      setState(() {
+                        selectedCountryCode = "+${country.phoneCode}";
+                        selectedCountryFlag = country.flagEmoji;
+                      });
+                    },
+                  );
+                },
+                child: Text('Pick country'),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(selectedCountryCode),
+                  SizedBox(width: 10),
+                  SizedBox(
+                    width: size.width * 0.7,
+                    child: TextField(
+                      controller: phoneController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        hint: Text('enter phone number'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.6),
+              Custombutton(text: 'Next', onPressed: () {}),
+            ],
+          ),
         ),
       ),
     );
